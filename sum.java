6561b9212d70889297ebac4c36a26e64f103a28e
iamPramodh <pramodhsiddam@gmail.com>
2020-01-28 11:03:00 +0530

    Devprogram1

---
+++ sum.java
@@ -0,0 +1,12 @@
+public class sum
+{
+	public static void main(String[] args)
+	{
+		int a,b, c;
+		a=10;
+		b=20;
+		c=20;
+		c=a+b+c;
+		System.out.println("Sum of 3 numbers: "+c);
+	}
+}
---
+++ sum1.java
@@ -0,0 +1,14 @@
+class {
+[B[A[B[A[A[A[A[A[B[A[B[A[B[A[B[A[B[B[B[C[C[C[C[C sum {[D[D[D[C1[C
+[A[Bpublic static void main(String args[]){
+
+ss
+asdga
+sd
+[A[B
+
+
+vim sum.javai
+
+exit
+
